71e39175a99eabbf1e7abc9f470d395f664ea171
vatraiadarsh <vatraiadarsh@gmail.com>
2018-08-08 17:42:05 +0545

    added frontend encore

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/home", name="home")
+     *  @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
